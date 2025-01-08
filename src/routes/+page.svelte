3e2573663e14e1bac6f28627bc25d4ea57cce54f
Slava Zakharov <90175094+petyapetr@users.noreply.github.com>
2025-01-09 02:30:54 +0400

    Add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
+<h1 class="text-amber-400">Welcome to SvelteKit</h1>
+<p class='text-6xl'>
 	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
 	documentation
 </p>
