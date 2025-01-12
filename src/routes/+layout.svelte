a6fde1cce2796730aa56cd7a1e2e9bf2d1517ace
Slava Zakharov <90175094+petyapetr@users.noreply.github.com>
2025-01-12 17:11:15 +0400

    Add theme switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,100 @@
 <script lang="ts">
 	import "$lib/css/app.css";
+	import {onMount} from "svelte";
 	import type {Snippet} from "svelte";
 
 	let {children}: {children: Snippet} = $props();
+
+	let currentTheme = $state("light");
+
+	onMount(() => {
+		setDefaultScheme();
+	});
+
+	function setDefaultScheme() {
+		const localStorageTheme = localStorage.getItem("theme");
+		if (localStorageTheme !== null) {
+			currentTheme = localStorageTheme;
+			return;
+		}
+
+		const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+		if (systemSettingDark.matches) {
+			currentTheme = "dark";
+		}
+
+		setTheme(currentTheme);
+	}
+
+	function toggleTheme() {
+		currentTheme = currentTheme === "light" ? "dark" : "light";
+		setTheme(currentTheme);
+	}
+
+	function setTheme(theme: string) {
+		document.querySelector("html")?.setAttribute("data-theme", theme);
+		localStorage.setItem("theme", theme);
+	}
 </script>
 
-{@render children()}
+<main class="grid grid-rows-main min-h-dvh">
+	<header class="px-4">
+		<a href="/" class="font-medium">Slava Zakharov</a>
+		<div>
+			<nav><ul></ul></nav>
+			<button type="button " onclick={toggleTheme}>
+				<svg class="fill-current size-4" viewBox="0 0 500 500"
+					><title>Change to {currentTheme} theme</title><circle
+						cx="248.48"
+						cy="252.55"
+						r="97.03"
+					></circle><rect height="112.39" width="27.9" x="234.53" y="17.45"
+					></rect><rect height="112.39" width="27.9" x="234.53" y="375.25"
+					></rect><rect
+						height="112.39"
+						transform="translate(679.92 -174.83) rotate(90)"
+						width="27.9"
+						x="413.42"
+						y="196.35"
+					></rect><rect
+						height="112.39"
+						transform="translate(322.12 182.97) rotate(90)"
+						width="27.9"
+						x="55.63"
+						y="196.35"
+					></rect><rect
+						height="112.39"
+						transform="translate(198.96 -228.23) rotate(45)"
+						width="27.9"
+						x="361.03"
+						y="69.85"
+					></rect><rect
+						height="112.39"
+						transform="translate(303.75 24.77) rotate(45)"
+						width="27.9"
+						x="108.03"
+						y="322.85"
+					></rect><rect
+						height="112.39"
+						transform="translate(908.15 381.93) rotate(135)"
+						width="27.9"
+						x="361.03"
+						y="322.85"
+					></rect><rect
+						height="112.39"
+						transform="translate(297.35 128.93) rotate(135)"
+						width="27.9"
+						x="108.03"
+						y="69.85"
+					></rect></svg
+				>
+			</button>
+		</div>
+	</header>
+
+	<div>
+		{@render children()}
+	</div>
+
+	<footer>footer</footer>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<h1 class="text-amber-400">Welcome to SvelteKit</h1>
-<p class='text-6xl'>
+<h1 class="text-amber-400 font-bold">Welcome to SvelteKit</h1>
+<p>
 	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
 	documentation
 </p>
